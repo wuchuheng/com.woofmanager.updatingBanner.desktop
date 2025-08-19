a5de2ce365e2306a687d77143f79f4c6fc799021
wuchuheng <root@wuchuheng.com>
2025-08-19 16:54:43 +0800

    inti

---
+++ webpack.main.config.ts
@@ -0,0 +1,24 @@
+import type { Configuration } from "webpack";
+import path from "path"
+
+import { plugins } from "./webpack.plugins";
+import { rules } from "./webpack.rules";
+
+export const mainConfig: Configuration = {
+  /**
+   * This is the main entry point for your application, it's the first file
+   * that runs in the main process.
+   */
+  entry: "./src/main/main.ts",
+  // Put your normal webpack config below here
+  module: {
+    rules,
+  },
+  plugins,
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+};
